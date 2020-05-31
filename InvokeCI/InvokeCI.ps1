f9340a78d749b77fdbe4371074697ea9131dcd18
Manuel Thalmann <m@nuth.ch>
2020-05-31 22:12:50 +0200

    Delete InvokeCI.ps1

--- InvokeCI/InvokeCI.ps1
+++
@@ -1,8 +0,0 @@
-function Invoke-CICommand
-{
-    if ($env:CONFIGURATION -eq "WSL") {
-        wsl $args[0] $args[1..$($args.Count - 1)];
-    } else {
-        . $args[0] $args[1..$($args.Count - 1)];
-    }
-}
