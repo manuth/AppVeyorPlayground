1db020339c8871d0b489a38764db23708ea3981d
Manuel Thalmann <m@nuth.ch>
2020-05-31 21:49:37 +0200

    Create InvokeCI.psm1

---
+++ InvokeCI.psm1
@@ -0,0 +1,8 @@
+function Invoke-CI-Command
+{
+    if ($env:CONFIGURATION -eq "WSL") {
+        wsl $args[0] $args[1..$($args.Count - 1)];
+    } else {
+        . $args[0] $args[1..$($args.Count - 1)];
+    }
+}
